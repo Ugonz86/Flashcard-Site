42ba46d15b72ba6a6824268f6b8c37f949fc96af
Uriel Gonzalez and Wei Dai <uriel@>
2019-10-22 11:35:37 -0700

    Add terms and definitions content to html with jquery functionality.

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#definitionJs").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable2").click(function() {
+    $("#definitionOp").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable3").click(function() {
+    $("#definitionVr").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable4").click(function() {
+    $("#definitionFn").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable5").click(function() {
+    $("#definitionMt").toggle();
+  });
+});
